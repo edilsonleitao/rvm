45cdfa0580e40228ec72624f4dc3f6bf24113eee
edilsonleitao <edilsonleitaolima@gmail.com>
2020-03-24 20:46:49 -0300

    feat: implementada nova tela de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,83 +1,86 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import { Container, LoginBox, Input, Btn, ButtonContainer } from "./styles";
-import { InputAdornment, IconButton } from "@material-ui/core";
-import { Visibility, VisibilityOff } from "@material-ui/icons";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
-import { BASE_URL } from "../../constants";
+import { Main } from "./styles";
 
-const Login = () => {
-  const history = useHistory();
-
-  const [values, setValues] = React.useState({
-    usuario: "",
-    senha: "",
-    mostraSenha: false
-  });
-
-  const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickmostraSenha = () => {
-    setValues({ ...values, mostraSenha: !values.mostraSenha });
-  };
-
-  const handleMouseDownPassword = event => {
-    event.preventDefault();
-  };
-
-  const handleClickEntrar = () =>
-    history.replace({
-      pathname: `${BASE_URL}/formularios`,
-      state: { fetchRemote: true }
-    });
-
+export default function SignIn() {
   return (
-    <Container>
-      <LoginBox>
-        <Input
-          color="primary"
-          size="small"
-          id="usuario"
-          margin="dense"
-          label="Usuário"
-          InputLabelProps={{ shrink: true }}
-          onChange={handleChange("usuario")}
-          fullWidth
-        />
-        <Input
-          color="primary"
-          margin="dense"
-          id="senha"
-          size="small"
-          label="Senha"
-          type={values.mostraSenha ? "text" : "password"}
-          onChange={handleChange("senha")}
-          InputLabelProps={{ shrink: true }}
-          fullWidth
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickmostraSenha}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.mostraSenha ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-        />
-        <ButtonContainer>
-          <Btn color="primary" variant="contained" onClick={handleClickEntrar}>
-            Entrar
-          </Btn>
-        </ButtonContainer>
-      </LoginBox>
-    </Container>
-  );
-};
+    <>
+      <CssBaseline />
 
-export default Login;
+      <Main>
+        <Container component="main" maxWidth="xs">
+          <Avatar>
+            <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            Entrar
+          </Typography>
+
+          <form>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Manter-me logado"
+            />
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              Entrar
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Esqueceu a senha?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Cadastre-se
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+
+          <Box mt={8}>
+            <Typography variant="body2" color="textSecondary" align="center">
+              {`Copyright © Salutis Tecnologia ${new Date().getFullYear()}.`}
+            </Typography>
+          </Box>
+        </Container>
+      </Main>
+    </>
+  );
+}
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import { TextField, Button } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 
-const Container = styled.div`
+export const Main = styled(Container)`
   margin: 0;
   padding: 0;
   border: 0;
@@ -12,37 +12,5 @@ const Container = styled.div`
   height: 100vh;
   align-items: center;
   justify-content: center;
-  background: linear-gradient(135deg, #5581b5 0%, #033c7c 100%);
+  background: linear-gradient(135deg, #ffffff 0%, #5581b5 100%);
 `;
-
-const LoginBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 280px;
-  height: 230px;
-  border-radius: 5%;
-  padding: 40px;
-  background: linear-gradient(
-    135deg,
-    #f6f8f9 0%,
-    #e5ebee 50%,
-    #d7dee3 51%,
-    #f5f7f9 100%
-  );
-`;
-
-const Input = styled(TextField)`
-  display: flex;
-  flex: 1;
-  margin: 10px;
-`;
-
-const ButtonContainer = styled.div`
-  margin-top: 20px;
-`;
-
-const Btn = styled(Button)``;
-
-export { Container, LoginBox, Input, Btn, ButtonContainer };
